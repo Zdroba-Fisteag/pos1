749f45a7db3b7c016135bd53d981266cc5e272d5
Armin-Rafael Zdroba-Fisteag <zdr240654@spengergasse.at>
2025-10-09 17:49:03 +0200

    FizzBuzzWhizzBang finished and TDOC-WIP

---
+++ Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java is doing its job");
+        TDOC first = new TDOC();
+        System.out.println(first.song());
+        // fizzbuzz();
+    }
+
+    public static void fizzbuzz() {
+        for (int i = 1; true; i++) {
+            String printString = "";
+            if (i % 3 == 0) {
+                printString = printString + "Fizz";
+                System.out.println(printString);
+            }
+            if (i % 5 == 0) {
+                printString = printString + "Buzz";
+                System.out.println(printString);
+            }
+            if (i % 7 == 0) {
+                printString = printString + "Whizz";
+                System.out.println(printString);
+            }
+            if (i % 11 == 0) {
+                printString = printString + "Bang";
+                System.out.println(printString);
+            }
+            if (printString.isEmpty()) {
+                System.out.println(i);
+            }
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0) {
+                break;
+            }
+        }
+    }
+}
---
+++ TDOC.java
@@ -0,0 +1,30 @@
+public class TDOC {
+    public static String song() {
+        String[] ftt = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
+                "eleventh", "twelfth" };
+        for (int i = 0; i <= 11; i++) {
+            // String onthe = "";
+            System.out.println("On the " + ftt[i] + " day of Christmas");
+            System.out.println("My true love sent to me:");
+            String[] goodluck = {
+                    "A partridge in a pear tree.",
+                    "Two turtle doves and",
+                    "Three french hens",
+                    "Four calling birds",
+                    "Five golden rings",
+                    "Six geese a-laying",
+                    "Seven swans a-swimming",
+                    "Eight maids a-milking",
+                    "Nine ladies dancing",
+                    "Ten lords a-leaping",
+                    "Eleven pipers piping",
+                    "Twelve drummers drumming"
+            };
+            System.out.println(goodluck[i]);
+            // for (int j = 0; j < 11; j++) {
+            // }
+            System.out.println();
+        }
+        return "Hello World";
+    }
+}

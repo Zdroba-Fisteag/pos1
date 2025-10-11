875bab81bca45243f748a749e2b6c3ebd0c83ad6
Armin-Rafael Zdroba-Fisteag <zdr240654@spengergasse.at>
2025-10-11 16:09:11 +0200

    TDOC - Nested for loop working - need to reverse the selection

--- TDOC.java
+++ TDOC.java
@@ -2,30 +2,32 @@ public class TDOC {
     public static String song() {
         String[] ftt = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
                 "eleventh", "twelfth" };
+        String[] goodluck = {
+                "A partridge in a pear tree.",
+                "Two turtle doves and",
+                "Three french hens",
+                "Four calling birds",
+                "Five golden rings",
+                "Six geese a-laying",
+                "Seven swans a-swimming",
+                "Eight maids a-milking",
+                "Nine ladies dancing",
+                "Ten lords a-leaping",
+                "Eleven pipers piping",
+                "Twelve drummers drumming"
+        };
         for (int i = 0; i <= 11; i++) {
             // String onthe = "";
             System.out.println("On the " + ftt[i] + " day of Christmas");
             System.out.println("My true love sent to me:");
-            String[] goodluck = {
-                    "A partridge in a pear tree.",
-                    "Two turtle doves and",
-                    "Three french hens",
-                    "Four calling birds",
-                    "Five golden rings",
-                    "Six geese a-laying",
-                    "Seven swans a-swimming",
-                    "Eight maids a-milking",
-                    "Nine ladies dancing",
-                    "Ten lords a-leaping",
-                    "Eleven pipers piping",
-                    "Twelve drummers drumming"
-            };
-            for (int j = 0; j <= 11; j++) {
+            // for (int j = 0; j <= 11; j++) {
+            for (int j = i; j <= 11; j++) {
                 String strang = "";
-                strang = strang + goodluck[j];
-                System.out.println(strang);
+                strang = strang + goodluck[j] + "\n";
+                System.out.print(strang);
             }
             System.out.println();
+            // }
         }
         return "Hello World";
     }
